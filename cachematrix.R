a0904e073bc1afe66d2f7edd5702643029ccd422
Rebecca Gore <rjgore@gmail.com>
2014-08-14 09:42:23 -0400

    functions for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are to store the inverse of a square full rank
+## matrix so that it will not rquire repeated computation since inverting
+## a matrix is a time consuming process.
 
-## Write a short comment describing this function
+## this function creates a list that contains four funtions that do:
+## 1) set the value of the full rank square matrix
+## 2) get the value of the matix
+## 3) set the value of the inverse of the matrix
+## 4) get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+       inv <- NULL
+       set <- function(y) {
+            x <<- y
+            inv <<- NULL
+       }
+       get <- function() x
+       setinv <- function(source) inv <<- source
+       getinv <- function() inv
+       list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse if it has not already been
+## calculated.  It checks to see if the inverse is already cached and
+## gets the inverse from the cache and skips the inversion calculation. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       inv <- x$getinv()
+       if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+       }
+       data <- x$get()
+       inv <- solve(data, ...)
+       x$setinv(inv)
+       inv
 }
+
